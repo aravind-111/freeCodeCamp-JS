05d2c8d327fdc0130588012bfcba7fad756f2d06
Aravindan-111 <aravindhan11195@gmail.com>
2022-03-09 21:11:49 +0530

    1st day commit

---
+++ index.js
@@ -0,0 +1,162 @@
+// 1
+
+    // This is an in-line comment.
+    /* This is a
+    multi-line comment */
+
+// 2
+
+    var myName;
+
+// 3
+
+    // Setup
+    var a;
+
+    // Only change code below this line
+
+    a=7;
+
+// 4
+
+    // Setup
+    var a;
+    a = 7;
+    var b;
+
+    // Only change code below this line
+
+    b = a;
+
+// 5
+
+    var a = 9;
+
+// 6
+
+    var myFirstName = "a";
+
+    var myLastName = "b";
+
+// 7
+
+    // Only change code below this line
+    var a = 5;
+    var b = 10;
+    var c = "I am a";
+    // Only change code above this line
+
+    a = a + 1;
+    b = b + 5;
+    c = c + " String!";
+
+// 8
+
+    // Variable declarations
+    var studlyCapVar;
+    var properCamelCase;
+    var titleCaseOver;
+
+    // Variable assignments
+    studlyCapVar = 10;
+    properCamelCase = "A String";
+    titleCaseOver = 9000;
+
+// 9
+
+    const catName = "Oliver";
+    const catSound = "Meow!";
+
+// 10
+
+    const FCC = "freeCodeCamp"; // Change this line
+    let fact = "is cool!"; // Change this line
+    fact = "is awesome!";
+    console.log(FCC, fact); // Change this line
+
+// 11
+
+    const sum = 10 + 10;
+
+// 12
+
+    const difference = 45 - 33;
+
+// 13
+
+    const product = 80 * 1;
+
+// 14
+
+    const quotient = 66 / 33;
+
+// 15
+
+    let myVar = 87;
+
+    // Only change code below this line
+    myVar = ++myVar;
+
+// 16
+
+    let myVarr = 11;
+
+    // Only change code below this line
+    myVarr = --myVarr;
+
+// 17
+
+    const myDecimal = 5.7;
+
+    // Only change code below this line
+    console.log(myDecimal);
+
+// 18
+
+    const pproduct = 2.5 * 2;
+
+// 19
+
+    const qquotient = 4.4 / 2.0; // Change this line
+
+// 20
+
+    let remainder;
+
+    remainder = 13 % 11;
+
+// 21
+
+    let a = 3;
+    let b = 17;
+    let c = 12;
+
+    // Only change code below this line
+    a += 12;
+    b += 8;
+    c += 7;
+
+// 13
+
+    let a = 11;
+    let b = 9;
+    let c = 3;
+
+    // Only change code below this line
+    a -= 6;
+    b -= 15;
+    c -= 1;
+
+// 14
+
+    let a = 5;
+    let b = 12;
+    let c = 4.6;
+
+    // Only change code below this line
+    a *= 5;
+    b *= 3;
+    c *= 10;
+
+// 15
+
