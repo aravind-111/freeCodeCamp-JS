b8d45ad4e9667b615a4ffbe1fdf5a363c22010b4
Aravindan-111 <aravindhan11195@gmail.com>
2022-03-11 14:46:44 +0530

    4th day

--- index.js
+++ index.js
@@ -450,3 +450,20 @@
 
 // 48
 
+    function nextInLine(arr, item) {
+        // Only change code below this line
+        // arr.push(item)
+        // return arr.length > 1 ? arr[0] : item;
+    
+        arr.push(item)
+        return arr.length>0 ?arr.shift(item):1;
+        // Only change code above this line
+    }
+    
+    // Setup
+    const testArr = [1, 2, 3, 4, 5];
+    
+    // Display code
+    console.log("Before: " + JSON.stringify(testArr));
+    console.log(nextInLine(testArr, 6));
+    console.log("After: " + JSON.stringify(testArr));
